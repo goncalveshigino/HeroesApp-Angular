0651ae6a979327ff746558d6c87b1a815461b871
goncalveshigino <goncalveshigino20@gmail.com>
2023-08-23 10:58:43 +0100

    feat: crud e conexao de formulario

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,7 +1,7 @@
 
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, catchError, of } from 'rxjs';
+import { Observable, catchError, map, of } from 'rxjs';
 
 import { environments } from '../../../environments/environments';
 import { Hero } from '../interfaces/hero.interface';
@@ -27,6 +27,24 @@ export class HeroesService {
 
   getSuggestions( query: string ): Observable<Hero[]> {
     return this.http.get<Hero[]>(`${ this.baseUrl }/heroes?q=${ query }&_limit=6`);
+  }
+
+  addHero( hero: Hero ): Observable<Hero> {
+    return this.http.post<Hero>(`${ this.baseUrl}/heroes`, hero );
+  }
+
+  updateHero( hero: Hero ): Observable<Hero> {
+    if( !hero.id ) throw Error('Hero id is required');
+
+    return this.http.patch<Hero>(`${ this.baseUrl}/heroe/${ hero.id }`, hero );
+  }
+
+  deleteHeroById( id: string ): Observable<boolean> {
+    return this.http.delete<Hero>(`${ this.baseUrl}/heroe/${ id }`)
+      .pipe(
+        catchError( error => of(false) ),
+        map( resp => true)
+      )
   }
 
 }
